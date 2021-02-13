c96717c3d2a414d1dfea330738ece6a40969021d
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-02-13 17:46:20 +0100

    Clear all Firestore entities for user when user is deleted

--- src/triggers/trackDeleteAcUser.ts
+++ src/triggers/trackDeleteAcUser.ts
@@ -6,6 +6,13 @@ import {
   deleteUserInternalConfig,
   getUserInternalConfig,
 } from '../repositories/userInternalConfigs';
+import { deleteUserExternalConfig } from '../repositories/userExternalConfigs';
+import { deleteCalendar, findCalendarsByUserId } from '../repositories/calendars';
+import {
+  deleteRecurringConfig,
+  findRecurringConfigsByUserId,
+} from '../repositories/recurringConfigs';
+import { deleteTask, findTasksByUserId } from '../repositories/tasks';
 
 // @link https://firebase.google.com/docs/functions/auth-events
 export default functions
@@ -15,26 +22,48 @@ export default functions
     const { uid } = user;
 
     const userInternalConfig = await getUserInternalConfig(uid);
-    if (!userInternalConfig) {
-      functions.logger.info('Skipping because user internal config not found', {
-        userId: user.uid,
-        userEmail: user.email,
-      });
-      return;
-    }
-    const { activeCampaignContactId } = userInternalConfig;
-    if (activeCampaignContactId) {
-      await deleteContact(activeCampaignContactId);
-      functions.logger.info('Deleted ActiveCampaign contact for user', {
-        userId: user.uid,
-        userEmail: user.email,
-        activeCampaignContactId,
-      });
+    if (userInternalConfig) {
+      const { activeCampaignContactId } = userInternalConfig;
+      if (activeCampaignContactId) {
+        await deleteContact(activeCampaignContactId);
+        functions.logger.info('Deleted ActiveCampaign contact for user', {
+          userId: user.uid,
+          userEmail: user.email,
+          activeCampaignContactId,
+        });
+      } else {
+        functions.logger.info('Skipping because no ActiveCampaign contact ID', {
+          userId: user.uid,
+          userEmail: user.email,
+        });
+      }
     } else {
-      functions.logger.info('Skipping because no ActiveCampaign contact ID', {
+      functions.logger.info('Skipping AC deletion because user internal config not found', {
         userId: user.uid,
         userEmail: user.email,
       });
     }
+
     await deleteUserInternalConfig(uid);
+    await deleteUserExternalConfig(uid);
+    const userCalendars = await findCalendarsByUserId(uid);
+    for (const [id] of userCalendars) {
+      await deleteCalendar(id);
+    }
+    const userRecurringConfigs = await findRecurringConfigsByUserId(uid);
+    for (const [id] of userRecurringConfigs) {
+      await deleteRecurringConfig(id);
+    }
+    const userTasks = await findTasksByUserId(uid);
+    for (const [id] of userTasks) {
+      await deleteTask(id);
+    }
+
+    functions.logger.info('Deleted user', {
+      userId: user.uid,
+      userEmail: user.email,
+      taskCount: userTasks.length,
+      recurringConfigCount: userRecurringConfigs.length,
+      calendarCount: userCalendars.length,
+    });
   });
