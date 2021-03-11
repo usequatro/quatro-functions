3c65505a12fee53e1de2380e6a2474119b5fcc19
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-03-11 22:16:24 +0100

    Comment out ActiveCampaign implementations

--- src/types/index.ts
+++ src/types/index.ts
@@ -42,108 +42,108 @@ export type OptionalKeys<T> = {
 };
 
 // Active Campaign types
-export type AcList = {
-  id: string;
-  name: string; // Development Contacts
-  stringid: string; // Eg. development-contacts
-};
+// export type AcList = {
+//   id: string;
+//   name: string; // Development Contacts
+//   stringid: string; // Eg. development-contacts
+// };
 
-export type AcListResponse = {
-  lists: AcList[];
-};
+// export type AcListResponse = {
+//   lists: AcList[];
+// };
 
-export type AcCustomField = {
-  id: string;
-  perstag: string; // Key in Active Campaign UI e.g. %CALENDAR%
-  title: string;
-  type: string; // text, textarea, date...
-};
+// export type AcCustomField = {
+//   id: string;
+//   perstag: string; // Key in Active Campaign UI e.g. %CALENDAR%
+//   title: string;
+//   type: string; // text, textarea, date...
+// };
 
-export type AcCustomFieldResponse = {
-  fields: AcCustomField[];
-};
+// export type AcCustomFieldResponse = {
+//   fields: AcCustomField[];
+// };
 
-export type AcTags = {
-  id: string;
-  tagType: string;
-  tag: string; // The tag name itself
-};
+// export type AcTags = {
+//   id: string;
+//   tagType: string;
+//   tag: string; // The tag name itself
+// };
 
-export type AcTagsResponse = {
-  tags: AcTags[];
-};
+// export type AcTagsResponse = {
+//   tags: AcTags[];
+// };
 
-export type AcContactTag = {
-  tag: string;
-  contact: string;
-  id: string;
-};
+// export type AcContactTag = {
+//   tag: string;
+//   contact: string;
+//   id: string;
+// };
 
-export type AcContactFieldValues = {
-  field: string | number;
-  value: string;
-};
+// export type AcContactFieldValues = {
+//   field: string | number;
+//   value: string;
+// };
 
-export type AcFieldValuesPayload = {
-  contact: string | number;
-} & AcContactFieldValues;
+// export type AcFieldValuesPayload = {
+//   contact: string | number;
+// } & AcContactFieldValues;
 
-export type AcContact = {
-  email: string;
-  firstName?: string;
-  phone?: string;
-};
+// export type AcContact = {
+//   email: string;
+//   firstName?: string;
+//   phone?: string;
+// };
 
-export type AcContactPayload = {
-  contact: {
-    fieldValues?: AcContactFieldValues[];
-  } & AcContact;
-};
+// export type AcContactPayload = {
+//   contact: {
+//     fieldValues?: AcContactFieldValues[];
+//   } & AcContact;
+// };
 
-export type AcContactResponse = {
-  contact: {
-    id: string;
-    email: string;
-    firstName?: string;
-    phone?: string;
-    fieldValues?: string[]; // Array of stored field values ids
-  };
-  fieldValues?: AcFieldValuesPayload[];
-};
+// export type AcContactResponse = {
+//   contact: {
+//     id: string;
+//     email: string;
+//     firstName?: string;
+//     phone?: string;
+//     fieldValues?: string[]; // Array of stored field values ids
+//   };
+//   fieldValues?: AcFieldValuesPayload[];
+// };
 
-export type AcContactListPayload = {
-  contactList: {
-    list: string | number;
-    contact: string | number;
-    status: number; // 0 unsuscribed, 1 suscribed
-  };
-};
+// export type AcContactListPayload = {
+//   contactList: {
+//     list: string | number;
+//     contact: string | number;
+//     status: number; // 0 unsuscribed, 1 suscribed
+//   };
+// };
 
-export type AcContactListResponse = {
-  contacts: AcContact[];
-} & AcContactListPayload;
+// export type AcContactListResponse = {
+//   contacts: AcContact[];
+// } & AcContactListPayload;
 
-export type AcFieldValuePayload = {
-  fieldValue: AcFieldValuesPayload;
-};
+// export type AcFieldValuePayload = {
+//   fieldValue: AcFieldValuesPayload;
+// };
 
-export type AcFieldValueResponse = {
-  contacts: AcContact[];
-} & AcFieldValuePayload;
+// export type AcFieldValueResponse = {
+//   contacts: AcContact[];
+// } & AcFieldValuePayload;
 
-export type AcTag = {
-  contact: string | number;
-  tag: string;
-};
+// export type AcTag = {
+//   contact: string | number;
+//   tag: string;
+// };
 
-export type AcContactTagPayload = {
-  contactTag: AcTag;
-};
+// export type AcContactTagPayload = {
+//   contactTag: AcTag;
+// };
 
-export type AcContactTagResponse = {
-  contactTag: AcContactTag;
-};
+// export type AcContactTagResponse = {
+//   contactTag: AcContactTag;
+// };
 
-export type AcContactTagListResponse = {
-  contactTags: AcContactTag[];
-};
+// export type AcContactTagListResponse = {
+//   contactTags: AcContactTag[];
+// };
--- src/utils/activeCampaignApi.ts
+++ src/utils/activeCampaignApi.ts
@@ -1,76 +1,76 @@
-import * as functions from 'firebase-functions';
-import { deleteRequest, getRequest, postRequest } from './fetch';
-import {
-  AcListResponse,
-  AcCustomFieldResponse,
-  AcTagsResponse,
-  AcContactPayload,
-  AcContactResponse,
-  AcContactListPayload,
-  AcContactListResponse,
-  AcFieldValuePayload,
-  AcFieldValueResponse,
-  AcContactTagPayload,
-  AcContactTagResponse,
-  AcContactTagListResponse,
-} from '../types';
+// import * as functions from 'firebase-functions';
+// import { deleteRequest, getRequest, postRequest } from './fetch';
+// import {
+//   AcListResponse,
+//   AcCustomFieldResponse,
+//   AcTagsResponse,
+//   AcContactPayload,
+//   AcContactResponse,
+//   AcContactListPayload,
+//   AcContactListResponse,
+//   AcFieldValuePayload,
+//   AcFieldValueResponse,
+//   AcContactTagPayload,
+//   AcContactTagResponse,
+//   AcContactTagListResponse,
+// } from '../types';
 
-const { url, key } = functions.config().activecampaign || {};
-if (!url) {
-  throw new Error('No env var activecampaign.url');
-}
-if (!key) {
-  throw new Error('No env var activecampaign.key');
-}
+// const { url, key } = functions.config().activecampaign || {};
+// if (!url) {
+//   throw new Error('No env var activecampaign.url');
+// }
+// if (!key) {
+//   throw new Error('No env var activecampaign.key');
+// }
 
-const buildUrl = (path: string): string => `${url}/api/3${path}`;
-const acHeaders: { [key: string]: string } = {
-  'Api-Token': key,
-};
+// const buildUrl = (path: string): string => `${url}/api/3${path}`;
+// const acHeaders: { [key: string]: string } = {
+//   'Api-Token': key,
+// };
 
-// @link ActiveCampaign v3 Reference: https://developers.activecampaign.com/reference
+// // @link ActiveCampaign v3 Reference: https://developers.activecampaign.com/reference
 
-export const getAllLists = async (): Promise<AcListResponse> =>
-  getRequest(buildUrl('/lists'), acHeaders) as Promise<AcListResponse>;
+// export const getAllLists = async (): Promise<AcListResponse> =>
+//   getRequest(buildUrl('/lists'), acHeaders) as Promise<AcListResponse>;
 
-export const getAllCustomFields = async (): Promise<AcCustomFieldResponse> =>
-  getRequest(buildUrl('/fields'), acHeaders) as Promise<AcCustomFieldResponse>;
+// export const getAllCustomFields = async (): Promise<AcCustomFieldResponse> =>
+//   getRequest(buildUrl('/fields'), acHeaders) as Promise<AcCustomFieldResponse>;
 
-export const getAllTags = async (): Promise<AcTagsResponse> =>
-  getRequest(buildUrl('/tags'), acHeaders) as Promise<AcTagsResponse>;
+// export const getAllTags = async (): Promise<AcTagsResponse> =>
+//   getRequest(buildUrl('/tags'), acHeaders) as Promise<AcTagsResponse>;
 
-export const createContact = async (contact: AcContactPayload): Promise<AcContactResponse> =>
-  postRequest(buildUrl('/contacts'), acHeaders, contact) as Promise<AcContactResponse>;
+// export const createContact = async (contact: AcContactPayload): Promise<AcContactResponse> =>
+//   postRequest(buildUrl('/contacts'), acHeaders, contact) as Promise<AcContactResponse>;
 
-export const deleteContact = async (id: string): Promise<AcContactResponse> =>
-  deleteRequest(buildUrl(`/contacts/${id}`), acHeaders) as Promise<AcContactResponse>;
+// export const deleteContact = async (id: string): Promise<AcContactResponse> =>
+//   deleteRequest(buildUrl(`/contacts/${id}`), acHeaders) as Promise<AcContactResponse>;
 
-export const addContactToList = async (
-  contactList: AcContactListPayload,
-): Promise<AcContactListResponse> =>
-  postRequest(buildUrl('/contactLists'), acHeaders, contactList) as Promise<AcContactListResponse>;
+// export const addContactToList = async (
+//   contactList: AcContactListPayload,
+// ): Promise<AcContactListResponse> =>
+//   postRequest(buildUrl('/contactLists'), acHeaders, contactList) as Promise<AcContactListResponse>;
 
-// Use this endpoint to add values to a Contact's Custom Field
-export const setCustomFieldValue = async (
-  fieldValue: AcFieldValuePayload,
-): Promise<AcFieldValueResponse> =>
-  postRequest(buildUrl('/fieldValues'), acHeaders, fieldValue) as Promise<AcFieldValueResponse>;
+// // Use this endpoint to add values to a Contact's Custom Field
+// export const setCustomFieldValue = async (
+//   fieldValue: AcFieldValuePayload,
+// ): Promise<AcFieldValueResponse> =>
+//   postRequest(buildUrl('/fieldValues'), acHeaders, fieldValue) as Promise<AcFieldValueResponse>;
 
-export const addTagToContact = async (
-  contactTag: AcContactTagPayload,
-): Promise<AcContactTagResponse> =>
-  postRequest(buildUrl('/contactTags'), acHeaders, contactTag) as Promise<AcContactTagResponse>;
+// export const addTagToContact = async (
+//   contactTag: AcContactTagPayload,
+// ): Promise<AcContactTagResponse> =>
+//   postRequest(buildUrl('/contactTags'), acHeaders, contactTag) as Promise<AcContactTagResponse>;
 
-export const getContactTagsForContact = async (
-  contactId: string,
-): Promise<AcContactTagListResponse> =>
-  getRequest(
-    buildUrl(`/contacts/${contactId}/contactTags`),
-    acHeaders,
-  ) as Promise<AcContactTagListResponse>;
+// export const getContactTagsForContact = async (
+//   contactId: string,
+// ): Promise<AcContactTagListResponse> =>
+//   getRequest(
+//     buildUrl(`/contacts/${contactId}/contactTags`),
+//     acHeaders,
+//   ) as Promise<AcContactTagListResponse>;
 
-export const deleteTagFromContact = async (contactTagId: string): Promise<AcContactTagResponse> =>
-  deleteRequest(
-    buildUrl(`/contactTags/${contactTagId}`),
-    acHeaders,
-  ) as Promise<AcContactTagResponse>;
+// export const deleteTagFromContact = async (contactTagId: string): Promise<AcContactTagResponse> =>
+//   deleteRequest(
+//     buildUrl(`/contactTags/${contactTagId}`),
+//     acHeaders,
+//   ) as Promise<AcContactTagResponse>;
