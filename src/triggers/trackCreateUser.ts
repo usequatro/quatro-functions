e3691d4da18ad968d7f4ffa06e134addb1d8abdc
Guillermo de la Puente <g.puente.allott@gmail.com>
2021-03-14 19:45:53 +0100

    Update onboarding tasks with Jon's copy

--- src/triggers/trackCreateUser.ts
+++ src/triggers/trackCreateUser.ts
@@ -6,22 +6,35 @@ import { create } from '../repositories/tasks';
 const ONBOARDING_TASKS = [
   {
     title: 'Connect Google Calendar',
-    description:
-      'Connecting your calendar will let you view your tasks and schedule side by side, and block time to give youself a chance to realistically plan your day',
+    description: 'Connecting your calendar will let you view your tasks and schedule side by side',
     effort: 0,
     impact: 4,
   },
   {
     title: 'Schedule recurring tasks',
-    description: 'Get all your daily or weekly rasks out of your head and onto your Top 4',
+    description: 'Get all of your daily or weekly tasks out of your head and into your Top 4',
     effort: 0,
     impact: 3,
   },
   {
     title: 'Block time for your first task',
-    description: 'Drag any task from your Top 4 into an open slot on your connected calendar',
+    description:
+      'After connecting your calendar, drag any task from your Top 4 into an open time slot to create a time block',
     effort: 0,
     impact: 2,
+  },
+  {
+    title: 'Add Quatro as a bookmark on your desktop',
+    description: '',
+    effort: 0,
+    impact: 1,
+  },
+  {
+    title: 'Add Quatro to your phone’s home screen',
+    description:
+      'For some helpful Quatro content, like how to create a native mobile app experience, check out our FAQ at https://usequatro.com/faq',
+    effort: 0,
+    impact: 0,
   },
 ];
 
